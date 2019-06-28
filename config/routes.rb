55721d695320c6c01e538cd0e2bedf9a28ac9a4a
chandru-techmango <chandrasekar.p@techmango.net>
2019-06-28 15:55:48 +0530

    basic login design

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'session/new'
   get 'home/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
+
+  resources :articles
+
 end
